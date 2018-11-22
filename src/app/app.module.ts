eedb93b8ea948d2ffc868f90af3d917cdc3b60d4
Christina Cannito <christinacannito@Christinas-MBP.home>
2018-11-21 21:54:54 -0500

    main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ResultsComponent } from './results/results.component';
+import { BirthdayFormComponent } from './birthday-form/birthday-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResultsComponent,
+    BirthdayFormComponent
   ],
   imports: [
     BrowserModule,
