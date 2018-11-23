02a8d4965f58ca230d8c32f0339dc8ce95172634
Christina Cannito <christinacannito@Christinas-MBP.home>
2018-11-23 10:53:03 -0500

    have basic form working

--- src/app/birthday-form/birthday-form.component.ts
+++ src/app/birthday-form/birthday-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NasaApiService } from '../nasa-api.service';
 
 @Component({
   selector: 'app-birthday-form',
@@ -9,15 +10,35 @@ export class BirthdayFormComponent implements OnInit {
   month: number;
   day: number;
   year: number;
+  imageUrl: string;
 
-  constructor() { }
+  constructor(private nasaApiService: NasaApiService) { }
 
   ngOnInit = () => {
+    
   }
 
-  submitBirthday = (monthInput, dayInput, yearInput) => {
-    this.month = monthInput;
-    this.day = dayInput;
-    this.year = yearInput
+  submitBirthday = () => {
+    // here you can make the call to the service 
+    let self = this;
+    this.nasaApiService.makeRequest(this.month, this.day, this.year).then((data) => {
+      console.log('data: ', typeof(data))
+      // make the request for the image
+      var converted = JSON.parse(data)
+      return converted
+    }, function(error) {
+      console.log('error: ', error)
+    }).then((birthdayData) => {
+      console.log('birthdayData: ', birthdayData)
+      // get the image from the birthday
+      // birthdayData = JSON.parse(birthdayData)
+      let imageName = birthdayData[0]['image']
+      console.log('imageName: ', typeof(birthdayData))
+      self.nasaApiService.imageRequest(imageName, self.day, self.month, self.year).then((imageUrl) => {
+        // console.log('imageUrl typeof: ', typeof(imageUrl))
+        // print the image to the screen 
+        self.imageUrl = imageUrl.toString();
+      })
+    })
   }
 }
--- src/app/nasa-api.service.ts
+++ src/app/nasa-api.service.ts
@@ -4,6 +4,75 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class NasaApiService {
+  url:string = "https://api.nasa.gov/EPIC/api/enhanced/date/";
+  key: string = "9JJprXB7NZaXke1ZjIyFwOOh5s6G0zXCIq79fZZ7";
+  // imageUrl: string = "https://epic.gsfc.nasa.gov/archive/enhanced/2015/11/22/png/epic_RGB_20151122001751.png";
+  imageUrl: string = "https://epic.gsfc.nasa.gov/archive/enhanced/"
+  // the url above is to get the actual image that you will find when you get the data back from the first request
 
-  constructor() { }
+  constructor() {}
+
+  makeRequest = (month, day, year) => {
+    let self = this;
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      // console.log('month: ', month, 'day: ', day, 'year: ', year)
+      let compiledUrl = self.url + year + '-' + month + '-' + day + '?api_key=' + self.key
+      console.log('compiled url: ', compiledUrl)
+      req.open('GET', compiledUrl);
+      req.onload = function() {
+        // This is called even on 404 etc
+        // so check the status
+        if (req.status == 200) {
+          // Resolve the promise with the response text
+          // console.log('req.response: ', req.response)
+          resolve(req.response);
+        }
+        else {
+          // Otherwise reject with the status text
+          // which will hopefully be a meaningful error
+          reject(Error(req.statusText));
+        }
+      };
+      // Handle network errors
+      req.onerror = function() {
+        reject(Error("Network Error"));
+      };
+      // Make the request
+      req.send();
+    });
+  }
+
+  imageRequest = (imageName, day, month, year) => {
+    let self = this;
+    return new Promise(function(resolve, reject) {
+      var req = new XMLHttpRequest();
+      // console.log('month: ', month, 'day: ', day, 'year: ', year)
+      //2015/11/22/png/epic_RGB_20151122001751.png
+      let imageDateUrl = self.imageUrl + year + '/' + month + '/' + day + '/png/' + imageName + '.png?api_key=' + self.key
+      console.log('compiled url: ', imageDateUrl)
+      
+      req.open('GET', imageDateUrl);
+      req.onload = function() {
+        // This is called even on 404 etc
+        // so check the status
+        if (req.status == 200) {
+          // Resolve the promise with the response text
+          // console.log('req.response: ', req.response)
+          resolve(imageDateUrl);
+        }
+        else {
+          // Otherwise reject with the status text
+          // which will hopefully be a meaningful error
+          reject(Error(req.statusText));
+        }
+      };
+      // Handle network errors
+      req.onerror = function() {
+        reject(Error("Network Error"));
+      };
+      // Make the request
+      req.send();
+    });
+  }
 }
